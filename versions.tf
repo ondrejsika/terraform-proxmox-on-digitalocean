a3003153d0226c51a726d0a228ca7e478fa7672e
Ondrej Sika <ondrej@ondrejsika.com>
2021-10-27 07:05:23 +0200

    chore: Remove hardcoded provider versi

--- versions.tf
+++ versions.tf
@@ -2,11 +2,9 @@ terraform {
   required_providers {
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "2.15.0"
     }
     digitalocean = {
       source  = "digitalocean/digitalocean"
-      version = "2.3.0"
     }
   }
   required_version = ">= 0.13"
