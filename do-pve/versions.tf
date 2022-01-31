77663ec8690f7ce4804975ec8a2cb32a069b7017
Ondrej Sika <ondrej@ondrejsika.com>
2022-01-31 07:49:56 +0100

    style: Reformat using terraform fmt

--- do-pve/versions.tf
+++ do-pve/versions.tf
@@ -1,10 +1,10 @@
 terraform {
   required_providers {
     cloudflare = {
-      source  = "cloudflare/cloudflare"
+      source = "cloudflare/cloudflare"
     }
     digitalocean = {
-      source  = "digitalocean/digitalocean"
+      source = "digitalocean/digitalocean"
     }
   }
   required_version = ">= 0.13"
