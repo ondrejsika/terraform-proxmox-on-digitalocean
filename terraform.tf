3faf3357b6e67f0958f5e5b40342dccb3597b0ce
Ondrej Sika <ondrej@ondrejsika.com>
2024-11-12 07:14:17 +0100

    feat: Create shared NFS on nfs.sikademo.com

--- terraform.tf
+++ terraform.tf
@@ -38,38 +38,6 @@ data "digitalocean_ssh_key" "ondrejsika" {
   name = "ondrejsika"
 }
 
-resource "digitalocean_droplet" "nfs" {
-  image  = "debian-12-x64"
-  name   = "pve-nfs-shared"
-  region = "nyc3"
-  size   = "s-1vcpu-2gb"
-  ssh_keys = [
-    data.digitalocean_ssh_key.ondrejsika.id,
-  ]
-  user_data = <<-EOF
-  #cloud-config
-  ssh_pwauth: yes
-  password: asdfasdf2020
-  chpasswd:
-    expire: false
-  runcmd:
-  - |
-    curl -fsSL https://raw.githubusercontent.com/sikalabs/slu/master/install.sh | sudo sh
-    apt-get install -y nfs-kernel-server
-    mkdir /nfs
-    echo '/nfs *(rw,no_root_squash)' > /etc/exports
-    systemctl restart nfs-kernel-server
-  EOF
-}
-
-resource "cloudflare_record" "nfs" {
-  zone_id = var.cloudflare_zone_id
-  name    = digitalocean_droplet.nfs.name
-  value   = digitalocean_droplet.nfs.ipv4_address
-  type    = "A"
-  proxied = false
-}
-
 module "pve" {
   for_each = local.clusters
 
